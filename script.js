0f6850773a34139dbffcf09b175cf5796b69004d
KarlapudiRakesh <160146429+KarlapudiRakesh@users.noreply.github.com>
2024-06-29 14:09:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY='api_key=e600e9d842e6ea9163fc6402b9b8bfbe';
+const API_KEY='api_key=30be86bcd005525d790b1cf37c746d92';
 const BASE_URL='https://api.themoviedb.org/3';
 const API_URL=BASE_URL +'/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL='https://image.tmdb.org/t/p/w500';
